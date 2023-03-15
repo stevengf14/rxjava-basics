8d2ed3286aa02448838dd19c695bc6c94e673da0
Steven <stevengf14@gmail.com>
2023-03-14 22:58:30 -0500

    Connectable Observable

---
+++ src/main/java/ec/com/learning/rxjava/calback/hotAndColdObservables/ConnectableObservable.java
@@ -0,0 +1,25 @@
+package ec.com.learning.rxjava.calback.hotAndColdObservables;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+import java.util.concurrent.TimeUnit;
+
+/**
+ *
+ * @author Steven Guamán - March 2023
+ */
+public class ConnectableObservable {
+
+    public static void main(String[] args) throws InterruptedException {
+        io.reactivex.rxjava3.observables.@NonNull ConnectableObservable<@NonNull Long> source = Observable.interval(1, TimeUnit.SECONDS).publish();
+
+        source.connect();
+
+        source.subscribe(System.out::println);
+
+        Thread.sleep(10000);
+        source.subscribe(System.out::println);
+
+        Thread.sleep(10000);
+    }
+}
