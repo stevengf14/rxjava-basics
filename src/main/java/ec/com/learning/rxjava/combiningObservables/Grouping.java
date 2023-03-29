873bde0e3ff34c60ebaac4f23f2fddaed13ad6d3
Steven <stevengf14@gmail.com>
2023-03-28 23:10:42 -0500

    Grouping

---
+++ src/main/java/ec/com/learning/rxjava/combiningObservables/Grouping.java
@@ -0,0 +1,33 @@
+package ec.com.learning.rxjava.combiningObservables;
+
+import ec.com.learning.rxjava.operators.Employee;
+import io.reactivex.rxjava3.core.Observable;
+
+/**
+ *
+ * @author Steven Guamán - March 2023
+ */
+public class Grouping {
+
+    public static void main(String[] args) {
+
+        Observable<Employee> obs = Observable.just(
+                new Employee(101, "Alexa", 60000, 4.0),
+                new Employee(123, "Dhwanit", 94000, 4.7),
+                new Employee(236, "Mike", 65000, 4.0),
+                new Employee(155, "Ella", 85000, 4.4),
+                new Employee(443, "George", 50000, 3.6),
+                new Employee(127, "Shreeja", 85000, 4.5),
+                new Employee(509, "Daniel", 60000, 4.0),
+                new Employee(344, "Lucy", 94000, 4.7),
+                new Employee(509, "Harry", 75000, 4.3),
+                new Employee(344, "Emma", 55000, 3.7)
+        );
+        obs.groupBy(e -> e.getRating())
+                //.flatMapSingle(e -> e.toMultimap(key -> e.getKey(), emp -> emp.getName()))
+                .flatMapSingle(e -> e.toList())
+                .subscribe(System.out::println);
+
+    }
+
+}
