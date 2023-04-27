804d2bb92b9b98165edc73130ce2236c13c40967
Steven <stevengf14@gmail.com>
2023-04-26 22:38:09 -0500

    Stream PipeLine

--- src/main/java/ec/com/learning/rxjava/Streams/ObservingTheStream.java
+++ src/main/java/ec/com/learning/rxjava/Streams/ObservingTheStream.java
@@ -14,22 +14,22 @@ public class ObservingTheStream {
     public static void main(String[] args) {
         List<Book> books = new ArrayList<>();
 
-        // before java 8
-        List<Book> popularHorrorBooks = books.stream()
-                .filter((book) -> book.getGenre().equalsIgnoreCase("Horror"))
-                .filter((book) -> book.getRating() > 3)
-                .collect(Collectors.toList());
+        // Stream the pipeline
+        List<Book> popularHorrorBooks = books.stream() // Source
+                .filter((book) -> book.getGenre().equalsIgnoreCase("Horror")) // Intermediate Operation
+                .filter((book) -> book.getRating() > 3) // Intermediate Operation
+                .collect(Collectors.toList()); // Terminal Operation
 
         // 1.
-        Stream<Book> stream = books.stream();
+        Stream<Book> stream = books.stream(); // Source
 
-        // 2.
+        // 2. Intermediate Operation
         Stream<Book> horrorBooks = stream.filter((book) -> book.getGenre().equalsIgnoreCase(("Horror")));
 
-        // 3.
+        // 3. Intermediate Operation
         Stream<Book> pHorrorBooks = horrorBooks.filter((book) -> book.getRating() > 3);
 
-        // 4.
+        // 4. Terminal Operation
         List<Book> collect = pHorrorBooks.collect(Collectors.toList());
     }
 
